2c9b4f7f9b6e944207898f1c7734f9175d807d33
bauti <bautistamendibe10@gmail.com>
2024-07-04 16:59:23 -0300

    feat: se agrega el model proveedor y el boton registrar nuevo proveedor

--- src/app/modules/proveedores/consultar-proveedores/consultar-proveedores.component.ts
+++ src/app/modules/proveedores/consultar-proveedores/consultar-proveedores.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {Proveedor} from "../../../models/proveedores.model";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-consultar-proveedores',
   templateUrl: './consultar-proveedores.component.html',
   styleUrl: './consultar-proveedores.component.scss'
 })
-export class ConsultarProveedoresComponent {
+export class ConsultarProveedoresComponent implements OnInit {
+
+  public tableDataSource: MatTableDataSource<Proveedor> = new MatTableDataSource<Proveedor>([]);
+  public form: FormGroup;
+  public proveedores: Proveedor[] = [];
+  public columnas: string[] = ['nombre', 'telefono', 'mail', 'direccion'];
+
+  constructor(private fb: FormBuilder) {
+    this.form = new FormGroup({});
+  }
+
+  ngOnInit() {
+    this.crearFormulario();
+  }
+
+  private crearFormulario() {
+    this.form = this.fb.group({
+      txNombre: ['', []],
+    });
+  }
+
+  public limpiarFiltros() {}
+
+  public buscar() {}
+
+  public registrarNuevoProveedor() {}
+
+
+  // Regios getters
+  get txNombre(): FormControl {
+    return this.form.get('txNombre') as FormControl;
+  }
 
 }
