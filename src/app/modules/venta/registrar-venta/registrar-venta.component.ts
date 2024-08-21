aaab8f3f5d4efdc980903eca751fc0f0ced80a61
bauti <bautistamendibe10@gmail.com>
2024-08-20 22:09:28 -0300

    se buscan los productos

--- src/app/modules/venta/registrar-venta/registrar-venta.component.ts
+++ src/app/modules/venta/registrar-venta/registrar-venta.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Producto} from "../../../models/producto.model";
+import {ProductosService} from "../../../services/productos.service";
+import {FiltrosProductos} from "../../../models/comandos/FiltrosProductos.comando";
 
 @Component({
   selector: 'app-registrar-venta',
   templateUrl: './registrar-venta.component.html',
   styleUrl: './registrar-venta.component.scss'
 })
-export class RegistrarVentaComponent {
-  isSelected: boolean = false;
+export class RegistrarVentaComponent implements OnInit{
+  public isSelected: boolean = false;
+  public productos: Producto[] = [];
+
+  constructor(private productosService: ProductosService) {
+  }
+
+  ngOnInit(){
+    this.buscarProductos();
+  }
+
+  private buscarProductos() {
+    const filtros: FiltrosProductos = new FiltrosProductos();
+    this.productosService.consultarProductos(filtros).subscribe((productos) => {
+      this.productos = productos;
+    });
+  }
 
   toggleSelection() {
     this.isSelected = !this.isSelected;
