30bef8d76926756f5cc6f4c479a2e9107a39df97
bauti <bautistamendibe10@gmail.com>
2024-10-05 18:34:55 -0300

    componente consultar ventas

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -22,7 +22,7 @@ export class SidebarComponent implements OnInit {
   menuItems: Menu[] = [
     {id: 1, nombre: 'Inicio', path:'', icon: 'home', activo: false, subMenu: []},
     {id: 2, nombre: 'Ventas', path:'registrar-venta', icon: 'sell', activo: false, subMenu: [
-        {id: 1, nombre: 'Registrar venta', path:'/registar-venta', icon: '', activo: false, subMenu: []},
+        {id: 1, nombre: 'Registrar venta', path:'/registrar-venta', icon: '', activo: false, subMenu: []},
         {id: 2, nombre: 'Consultar ventas', path:'/consultar-ventas', icon: '', activo: false, subMenu: []}
       ]},
     {id: 3, nombre: 'Ordenes de Compra', path:'consultar-pedidos', icon: 'receipt', activo: false, requiresAdmin: true, subMenu: []},
