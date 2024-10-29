5d0dd28d8ec59369cf9bf374ae750d505be01f57
bauti <bautistamendibe10@gmail.com>
2024-10-29 13:46:54 -0300

    mapear idafip en forma de pago

--- src/app/models/formaDePago.model.ts
+++ src/app/models/formaDePago.model.ts
@@ -1,9 +1,11 @@
 export class FormaDePago {
   id: number;
   nombre: string;
+  idAfip: number;
 
-  constructor(id?: number, nombre?: string) {
+  constructor(id?: number, nombre?: string, idAfip?: number) {
     this.id = id!;
     this.nombre = nombre!;
+    this.idAfip = idAfip!;
   }
 }
