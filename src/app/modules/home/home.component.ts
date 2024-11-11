38c90fc89d10024c46a63d94611f9de2d110ef4f
bauti <bautistamendibe10@gmail.com>
2024-11-11 19:23:57 -0300

    componente ventas de hoy

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
 
-  public obtenerClaseCantidadVentaDiaAnterior() {
-    return 'mayorAlDiaAnterior';
+  ngOnInit() {
+    this.buscarTotalVentasHoy();
+  }
+
+  private buscarTotalVentasHoy() {
+
   }
 
 }
