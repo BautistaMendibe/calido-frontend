be8e784a50391e789ecad9c2ce5531eeb63c68d7
bauti <bautistamendibe10@gmail.com>
2024-11-11 21:56:03 -0300

    obtener ventas hoy y ayer

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {VentasService} from "../../services/ventas.services";
+import {Venta} from "../../models/venta.model";
 
 @Component({
   selector: 'app-home',
@@ -7,13 +9,44 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit{
 
+  public cantidadVentasHoy = 0;
+  public cantidadVentasAyer = 0;
+  public diferenciaCantidadVentas = 0;
+
+  private ventasHoy: Venta[] = [];
+  private ventasAyer: Venta[] = [];
+
+
+  constructor(
+      private ventasService: VentasService
+  ) {
+  }
+
 
   ngOnInit() {
     this.buscarTotalVentasHoy();
   }
 
   private buscarTotalVentasHoy() {
+    const hoy = new Date();
+    const ayer = new Date(hoy);
+    ayer.setDate(hoy.getDate() - 1);
 
+
+    this.ventasService.buscarVentasFechaHora(hoy as unknown as string).subscribe((ventas) => {
+      this.ventasHoy = ventas;
+      this.cantidadVentasHoy = ventas.length;
+
+      this.ventasService.buscarVentasFechaHora(ayer as unknown as string).subscribe((ventas) => {
+        this.ventasAyer = ventas;
+        this.cantidadVentasAyer = ventas.length;
+
+        this.calcularDiferenciaVentasAyerHoy();
+      });
+
+    });
   }
+
+  private calcularDiferenciaVentasAyerHoy() {}
 
 }
