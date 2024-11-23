d37cacb07ee85717c3a62f9fe446480072822cc7
bauti <bautistamendibe10@gmail.com>
2024-11-23 17:32:37 -0300

    componente de asistencia empleados

--- src/app/modules/home/asistencias-empleados/asistencias-empleados.component.ts
+++ src/app/modules/home/asistencias-empleados/asistencias-empleados.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Asistencia} from "../../../models/asistencia";
+import {UsuariosService} from "../../../services/usuarios.service";
+import {FiltrosAsistencias} from "../../../models/comandos/FiltrosAsistencias.comando";
 
 @Component({
   selector: 'app-asistencias-empleados',
   templateUrl: './asistencias-empleados.component.html',
   styleUrl: './asistencias-empleados.component.scss'
 })
-export class AsistenciasEmpleadosComponent {
+export class AsistenciasEmpleadosComponent implements OnInit {
+
+  public asistencias: Asistencia[] = [];
+  private filtrosAsistencias: FiltrosAsistencias;
+  public buscando: boolean = false;
+
+  constructor(private usuariosService: UsuariosService) {
+    this.filtrosAsistencias = new FiltrosAsistencias();
+  }
+
+  ngOnInit() {
+    this.buscarAsistenciasHoy();
+  }
+
+  private buscarAsistenciasHoy() {
+    this.filtrosAsistencias.fecha = new Date();
+    this.buscando = true;
+
+    this.usuariosService.consultarAsistencias(this.filtrosAsistencias).subscribe((asistencias) => {
+      this.asistencias = asistencias;
+      this.buscando = false;
+    });
+  }
 
 }
