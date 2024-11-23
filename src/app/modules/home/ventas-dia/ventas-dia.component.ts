4c10db6eb13bee5c36a48238e8a60d231fdbf2d3
bauti <bautistamendibe10@gmail.com>
2024-11-23 13:24:10 -0300

    graficos ventas diarias

--- src/app/modules/home/ventas-dia/ventas-dia.component.ts
+++ src/app/modules/home/ventas-dia/ventas-dia.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Venta} from "../../../models/venta.model";
 import {VentasService} from "../../../services/ventas.services";
+import {Chart, ChartDataset, ChartOptions, registerables} from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-ventas-dia',
@@ -44,5 +47,90 @@ export class VentasDiaComponent implements OnInit{
   }
 
   private calcularDiferenciaVentasAyerHoy() {}
+
+
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        ticks: {
+          font: {
+            size: 10,
+          },
+          maxRotation: 0,
+          minRotation: 0,
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.1)',
+        },
+      },
+      y: {
+        ticks: {
+          font: {
+            size: 10,
+          },
+          padding: 10,
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.1)',
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: true,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        titleColor: '#0e0e0e',
+        bodyColor: '#bc7777',
+      },
+    },
+    elements: {
+      line: {
+        tension: 0.4,
+        borderWidth: 2,
+      },
+      point: {
+        radius: 0,
+        hoverRadius: 5,
+      },
+    },
+    layout: {
+      padding: {
+        top: 10,
+        left: 10,
+        right: 10,
+        bottom: 10,
+      },
+    },
+  };
+
+  public lineChartLabels: string[] = [
+    '8:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00',
+  ];
+
+  public lineChartData: ChartDataset<'line', number[]>[] = [
+    {
+      type: 'line',
+      data: [200, 300, 400, 500, 573, 600, 550, 500],
+      label: 'Today',
+      borderColor: 'rgba(225, 91, 53, 1)',
+      backgroundColor: 'rgb(85,24,5)',
+      borderWidth: 2,
+      fill: false,
+    },
+    {
+      type: 'line',
+      data: [150, 250, 350, 400, 451, 480, 470, 420],
+      label: 'Yesterday',
+      borderColor: 'rgba(128, 128, 128, 1)',
+      backgroundColor: 'rgba(128, 128, 128, 0.1)',
+      borderWidth: 2,
+      fill: false,
+    },
+  ];
 
 }
