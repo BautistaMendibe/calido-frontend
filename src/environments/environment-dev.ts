b9865071990b2bb9f9d6d7de01662abe9ed2cb61
bauti <bautistamendibe10@gmail.com>
2024-12-05 01:07:04 -0300

    variable backend prod

--- src/environments/environment-dev.ts
+++ src/environments/environment-dev.ts
@@ -1,5 +1,5 @@
 export const environmentDEV = {
   production: true,
-  backendUrl: 'spare-toinette-calido-841cee5f.koyeb.app',
+  backendUrl: 'https://spare-toinette-calido-841cee5f.koyeb.app',
   //backendUrl: 'calido-backend.vercel.app'
 };
