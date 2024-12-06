3a173d6dc983e900180f0af730d9aba1c5ec0ecb
Julián <julicarles9@gmail.com>
2024-12-05 22:23:43 -0300

    fix: consistencia en reportes
    - Cambiamos "Estadísticas" en sidebar por "Análisis de datos"
    - Si "Reportes" es el defecto al dar click, que esté primero ese y no las estadísticas
    - error id duplicado html de reportes

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -36,9 +36,9 @@ export class SidebarComponent implements OnInit {
       ]},
     {id: 6, nombre: 'Productos', path:'consultar-productos', icon: 'shopping_cart', activo: false, subMenu: []},
     {id: 7, nombre: 'Proveedores', path:'consultar-proveedores', icon: 'local_shipping', activo: false, subMenu: []},
-    {id: 8, nombre: 'Estadisticas', path:'generar-reportes', icon: 'data_usage', activo: false, subMenu: [
-      {id: 1, nombre: 'visualizaciones', path:'visualizaciones', icon: '', activo: false, subMenu: []},
-      {id: 2, nombre: 'reportes', path:'generar-reportes', icon: '', activo: false, subMenu: []}
+    {id: 8, nombre: 'Análisis de datos', path:'generar-reportes', icon: 'data_usage', activo: false, subMenu: [
+      {id: 1, nombre: 'reportes', path:'generar-reportes', icon: '', activo: false, subMenu: []},
+      {id: 2, nombre: 'estadísticas', path:'visualizaciones', icon: '', activo: false, subMenu: []}
     ]},
     {id: 9, nombre: 'Empleados', path:'consultar-empleados', icon: 'supervisor_account', activo: false, subMenu: [
         {id: 1, nombre: 'Consultar empleados', path:'consultar-empleados', icon: '', activo: false, subMenu: []},
