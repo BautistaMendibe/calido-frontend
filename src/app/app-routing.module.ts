af941fab1756b0ce2fce2456b3adf0329b0b5403
Julián <julicarles9@gmail.com>
2024-12-07 19:41:19 -0300

    fix: adm no puede hacer ventas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -115,7 +115,7 @@ const routes: Routes = [
     path: 'registrar-venta',
     component: RegistrarVentaComponent,
     canActivate: [authGuard],
-    data: { roles: ['Cajero', 'Administrador', 'Encargado'] },
+    data: { roles: ['Cajero', 'Encargado'] },
   },
   {
     path: 'consultar-pedidos',
