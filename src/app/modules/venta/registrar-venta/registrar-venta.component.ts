5c7a8e7666fe2a65316bd7e85f9940eaae5a8a27
bauti <bautistamendibe10@gmail.com>
2025-01-30 18:31:45 -0300

    cambio color progress bar

--- src/app/modules/venta/registrar-venta/registrar-venta.component.ts
+++ src/app/modules/venta/registrar-venta/registrar-venta.component.ts
@@ -660,21 +660,22 @@ export class RegistrarVentaComponent implements OnInit{
   }
 
   public registrarProducto() {
-    this.dialog.open(
-      RegistrarProductoComponent,
-      {
-        width: '90%',
-        height: 'auto',
-        autoFocus: false,
-        maxHeight: '80vh',
-        panelClass: 'dialog-container',
-        data: {
-          referencia: this,
-          esConsulta: false,
-          formDesactivado: false
-        }
-      }
-    );
+    const snackBarRef = this.notificacionService.openSnackBarLoading();
+    //this.dialog.open(
+    //  RegistrarProductoComponent,
+    //  {
+    //    width: '90%',
+    //    height: 'auto',
+    //    autoFocus: false,
+    //    maxHeight: '80vh',
+    //    panelClass: 'dialog-container',
+    //    data: {
+    //      referencia: this,
+    //      esConsulta: false,
+    //      formDesactivado: false
+    //    }
+    //  }
+    //);
   }
 
   public registrarCliente() {
