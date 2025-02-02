bd3098363d1202f95f44fa1bb1ad61c558225319
Julián <julicarles9@gmail.com>
2025-02-02 16:45:57 -0300

    fix: autocomplete en ord compra

--- src/app/modules/pedidos/consultar-pedidos/consultar-pedidos.component.ts
+++ src/app/modules/pedidos/consultar-pedidos/consultar-pedidos.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {MatTableDataSource} from "@angular/material/table";
-import {Form, FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {MatDialog} from "@angular/material/dialog";
 import {Router} from "@angular/router";
 import {SnackBarService} from "../../../services/snack-bar.service";
